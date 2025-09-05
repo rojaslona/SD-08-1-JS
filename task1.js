154d8e03ba5acdd5fd92b341509e89931d86d9ad
rojaslona <rojaslona@outlook.com>
2025-09-05 14:34:10 -0600

    task finished

--- task1.js
+++ task1.js
@@ -1,7 +1,18 @@
 export class Player {
-    constructor() {
-      
+  name
+  level
+    constructor(name, level) {
+      this.name = name
+      this.level = level
     }
-  
-    
+  info() {
+      return `${this.name} has reached level ${this.level}`
+    }
+  levelUp() {
+      return `${this.name} has leveled up to level ${++this.level}`
+    }
   }
+
+  const player1 = new Player('Tara', 6)
+  console.log(player1.info())
+  console.log(player1.levelUp())  
